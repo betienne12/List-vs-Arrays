02558c84e0f74d2914d13316d90689ebc444c885
Brian Etienne <betienne@umassd.edu>
2017-04-11 21:46:39 -0400

    insertions

--- list v Arrays/list v Arrays/Program.cs
+++ list v Arrays/list v Arrays/Program.cs
@@ -10,6 +10,20 @@ namespace list_v_Arrays
     {
         static void Main(string[] args)
         {
+            string line;
+            int cnt=0;
+            System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\Brian\\Desktop\\dictionaries\\1k.txt");
+            LinkedList<string> linked = new LinkedList<string>();
+            while ((line = file.ReadLine()) != null)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                linked.AddLast(line);
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+
+            }
+            Console.WriteLine(elapsedMs);
+            file.Close();
         }
-    }
-}
+     }
+   }

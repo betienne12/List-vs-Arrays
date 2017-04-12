9ced6656588c36efca9bcab42e3e80ab015e99df
Brian Etienne <betienne@umassd.edu>
2017-04-11 23:36:48 -0400

    sstuff

--- list v Arrays/list v Arrays/Program.cs
+++ list v Arrays/list v Arrays/Program.cs
@@ -13,16 +13,63 @@ namespace list_v_Arrays
         static void Main(string[] args)
         {
             string line;
-            System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\Brian\\Desktop\\dictionaries\\50k.txt");
+            System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\Brian\\Desktop\\dictionaries\\test.txt");
             LinkedList<string> linked = new LinkedList<string>();
+            List<string> linked2 = new List<string>();
             Stopwatch sw = new Stopwatch();
+            string value = "sort"; 
             //inserting into linked list
-            while ((line = file.ReadLine()) != null)
+            switch (value)
             {
-                sw.Start();
-                linked.AddLast(line);              
+                case("insert"):
+                    {
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            sw.Start();
+                            linked.AddLast(line);
+                        }
+                        sw.Stop();
+                        break;
+                    }
+                case ("delete"):
+                    {
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            sw.Start();
+                            linked.Remove(line);
+                        }
+
+                        sw.Stop();
+                        break;
+                    }
+                case ("sort"):
+                    {
+                        
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            
+                            linked.AddLast(line);
+                        }
+                        sw.Start();
+                        //linked2 = linked.OrderByDescending().ToList();
+     
+                        sw.Stop();
+                        foreach (string s in linked2)
+                        {
+                            Console.WriteLine(s);
+                        }
+                        break;
+                    }
+                case ("search"):
+                    {
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            linked.AddLast(line);
+                        }
+                        linked.to
+                        break;
+                    }
             }
-            sw.Stop();
             string ExecutionTimeTaken = string.Format("Minutes :{0}\nSeconds :{1}\n Mili seconds :{2}", sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.TotalMilliseconds);
             int c = linked.Count();
             Console.WriteLine("There are {0} items", c);

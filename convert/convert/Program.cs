36fdb8d184d50d791a5831f899e3f0790bf0c224
Brian Etienne <betienne@umassd.edu>
2017-04-17 23:21:31 -0400

    did linked list sorting

---
+++ convert/convert/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            string low;
+            string file_size = "test";  //test, 1k, 10k, 50k, 100k
+            System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\Brian\\Desktop\\dictionaries\\" + file_size + ".txt");
+
+            string path = "C:\\Users\\Brian\\Desktop\\dictionaries\\Test_2.txt";
+            System.IO.StreamWriter output = new System.IO.StreamWriter(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                low = line.ToLower();
+                output.WriteLine(low);
+            }
+            output.Close();
+            file.Close();
+            File.WriteAllLines(path, File.ReadAllLines(path).Where(l => !string.IsNullOrWhiteSpace(l)));
+        }
+    }
+}
--- list v Arrays/list v Arrays/Program.cs
+++ list v Arrays/list v Arrays/Program.cs
@@ -13,24 +13,25 @@ namespace list_v_Arrays
         static void Main(string[] args)
         {
             string line;
-            string file_size = "test";  //test, 1k, 10k, 50k, 100k
-             System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\Brian\\Desktop\\dictionaries\\"+ file_size+ ".txt");
+            string file_size = "Test_2";  //test, 1k, 10k, 50k, 100k
+
+            System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\Brian\\Desktop\\dictionaries\\" + file_size + ".txt");
             LinkedList<string> linked = new LinkedList<string>();
-            string [] array = new string[4];
+            string[] array = new string[4];
             List<string> linked2 = new List<string>();
             Stopwatch sw = new Stopwatch();
 
 
             string structure = "linked";  //linked or array
-            string action = "insert";     //insert, delete, sort, search
+            string action = "sort";     //insert, delete, sort, search
             string datatype = "string";  //string or int
-            string value = structure + "_"+ action+"_"+ datatype;
+            string value = structure + "_" + action + "_" + datatype;
             Console.WriteLine(value);
-            
+
             //inserting into linked list
             switch (value)
             {
-                case("linked_insert_string"):
+                case ("linked_insert_string"):
                     {
                         while ((line = file.ReadLine()) != null)
                         {
@@ -53,21 +54,48 @@ namespace list_v_Arrays
                     }
                 case ("linked_sort_string"):
                     {
-                        
+
                         while ((line = file.ReadLine()) != null)
                         {
-                            
+
                             linked.AddLast(line);
+
                         }
                         sw.Start();
-                        //linked2 = linked.OrderByDescending().ToList();
-     
-                        sw.Stop();
-                        foreach (string s in linked2)
+                        int i, j, k;
+                        string tempData;
+                        LinkedListNode<string> next = null;
+                        LinkedListNode<string> curr = null;
+
+                        int size = linked.Count;
+                        k = size;
+
+                        for (i = 0; i < size - 1; i++, k--)
                         {
-                            Console.WriteLine(s);
+                            curr = linked.First;
+                            next = linked.First.Next;
+
+                            for (j = 1; j < k; j++)
+                            {
+
+                                if (curr.Value[0] > next.Value[0])
+                                {
+                                    tempData = curr.Value;
+                                    curr.Value = next.Value;
+                                    next.Value = tempData;
+                                }
+
+                                curr = curr.Next;
+                                next = next.Next;
+                            }
+                        }
+                        sw.Stop();
+                        foreach (string item in linked)
+                        {
+                            Console.WriteLine(item);
                         }
                         break;
+
                     }
                 case ("linked_search_string"):
                     {
@@ -77,7 +105,7 @@ namespace list_v_Arrays
                         }
                         linked2 = linked.ToList();
 
-                        foreach (string  word in linked2)
+                        foreach (string word in linked2)
                         {
                             sw.Start();
                             linked.Find(word);
@@ -87,20 +115,31 @@ namespace list_v_Arrays
                     }
                 case ("array_insert_string"):
                     {
+                        int i = 0;
                         while ((line = file.ReadLine()) != null)
                         {
-                            for(int i=0; i<=array.Length; i++)
-                            {
-                                array[i] = line;
-                            }
                             sw.Start();
-                            
+                            array[i] = line;
+                            i++;
                         }
-
+                        sw.Stop();
                         break;
                     }
                 case ("array_delete_string"):
                     {
+                        int i = 0;
+                        int x = 0;
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            array[i] = line;
+                            i++;
+
+                        }
+                        sw.Start();
+                        for (x = 0; x < array.Length; x++)
+                            array[x] = null;
+                        sw.Stop();
+
                         break;
                     }
                 case ("array_search_string"):
@@ -109,15 +148,20 @@ namespace list_v_Arrays
                     }
                 case ("array_sort_string"):
                     {
+
                         break;
                     }
             }
-            string ExecutionTimeTaken = string.Format("Minutes :{0}\nSeconds :{1}\n Mili seconds :{2}", sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.TotalMilliseconds);
-            int c = linked.Count();
-            Console.WriteLine("There are {0} items", c);
-            Console.WriteLine(ExecutionTimeTaken);
-            sw.Reset();
-            file.Close();
+
+
+                    string ExecutionTimeTaken = string.Format("Minutes :{0}\nSeconds :{1}\n Mili seconds :{2}", sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.TotalMilliseconds);
+                    int c = linked.Count();
+                    int d = array.Count();
+                    Console.WriteLine("There are {0} items in the linked list", c);
+                    Console.WriteLine("There are {0} items in the array", d);
+                    Console.WriteLine(ExecutionTimeTaken);
+                    sw.Reset();
+                    file.Close();
+            }
         }
-     }
-   }
+    }
